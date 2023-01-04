3c446fd28dc60e187b85da2d84d3ced8b62b421b
Teror Fox <99103591+sysfox@users.noreply.github.com>
2023-01-04 14:20:23 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,12 +1,12 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'IMSYY 站点监测',
+  SiteName: 'Teror的站点监测',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'u1022423-6ffcda436b183008644c691c',
+    'ur1710837-33e2ae3aa58bc58e54e4cd28',
   ],
 
   // 日志天数
@@ -19,15 +19,15 @@ window.Config = {
   Navi: [
     {
       text: '主页',
-      url: 'https://www.imsyy.top/'
+      url: 'https://www.trfox.ml/'
     },
     {
       text: '博客',
-      url: 'https://blog.imsyy.top/'
+      url: 'https://blog.trfox.ml/'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/imsyy/'
+      url: 'https://github.com/sysfox/'
     },
   ],
 };
